023314a795977fa78e455926aa52cf6422f91a1f
Abdillahzakkie <zakariyyaopeyemi@gmail.com>
2020-10-25 10:11:53 +0100

    First commit

---
+++ precache-manifest.30d82c31a9826cabea41d4235b97dd4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b597128cf6bb95aac57dfec3a076a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2554a7c91553073b2914",
+    "url": "/static/css/main.6d6cd067.chunk.css"
+  },
+  {
+    "revision": "56db902f13bebe9a8509",
+    "url": "/static/js/2.2f2c82d7.chunk.js"
+  },
+  {
+    "revision": "103883be697ef591c817e5ef97e6a189",
+    "url": "/static/js/2.2f2c82d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2554a7c91553073b2914",
+    "url": "/static/js/main.7dac65bf.chunk.js"
+  },
+  {
+    "revision": "ab2d8da4be890a560f88",
+    "url": "/static/js/runtime-main.249a1e13.js"
+  },
+  {
+    "revision": "0f1b4d6010b15e50387ef4679fa76147",
+    "url": "/static/media/62_624227_puppy_cute_adorable_digital_cartoon_dog_animal_puppy.0f1b4d60.png"
+  },
+  {
+    "revision": "a6e7d2d67b76627fec97f561490a3b19",
+    "url": "/static/media/chef.a6e7d2d6.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  }
+]);
